09ae13eb1bed0b3b6924cf712b89a02aa4092889
Conor Higgins <wodowods@gmail.com>
2022-09-22 23:42:56 +0100

    added projects to portfolio

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,7 +1,19 @@
+import paletteGif from "../Assets/paletteGen.gif";
 import waveDashGif from "../Assets/waveDash_gif_1.gif";
 import calculatorPNG from "../Assets/calculator_1.gif";
 import mountainsGif from "../Assets/mountains_1.gif";
 import punkGif from "../Assets/punkAPI_1.gif";
+import ticketGif from "../Assets/ticketSystem.gif";
+import hangmanPNG from "../Assets/hangman.png";
+import snapPNG from "../Assets/snap.png";
+
+export const palette = {
+    image: paletteGif,
+    title: "Colour Palette Generator",
+    tag: "Full stack React/Java/SQL app",
+    link: "#",
+};
+
 export const waveDash = {
     image: waveDashGif,
     title: "waveDash",
@@ -26,8 +38,38 @@ export const mountains = {
 export const punkAPI = {
     image: punkGif,
     title: "Brewdog Punk API",
-    tag: "React-based Api",
+    tag: "React API app",
     link: "https://conhiggins.github.io/oxnaPunkAPI",
 };
 
-export const projects = [waveDash, calculator, mountains, punkAPI];
+export const ticketSystem = {
+    image: ticketGif,
+    title: "Basic ticketing system",
+    tag: "React app",
+    link: "https://conhiggins.github.io/oxnaTicketSystem",
+};
+
+export const hangman = {
+    image: hangmanPNG,
+    title: "Hangman terminal game",
+    tag: "Java terminal app",
+    link: "#",
+};
+
+export const snap = {
+    image: snapPNG,
+    title: "Snap terminal game",
+    tag: "Java terminal app",
+    link: "#",
+};
+
+export const projects = [
+    palette,
+    waveDash,
+    calculator,
+    mountains,
+    punkAPI,
+    ticketSystem,
+    hangman,
+    snap,
+];
